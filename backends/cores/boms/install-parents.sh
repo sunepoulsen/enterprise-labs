3f3a1062d2d3db0c020168559476dc5c7fca699e
sunepoulsen <sunepoulsen@gmail.com>
2020-03-14 13:04:53 +0100

    Added common boms for services.

---
+++ backends/cores/boms/install-parents.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mvn -N install:install-file -Dpackaging=pom -Dfile=../../pom.xml -DpomFile=../../pom.xml
+mvn -N install:install-file -Dpackaging=pom -Dfile=../pom.xml -DpomFile=../pom.xml
+mvn -N install:install-file -Dpackaging=pom -Dfile=pom.xml -DpomFile=pom.xml
+mvn -N install:install-file -Dpackaging=pom -Dfile=service/pom.xml -DpomFile=service/pom.xml
