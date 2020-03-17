709993009d37d0a46662a427eabf5b53ef0c6a83
sunepoulsen <sunepoulsen@gmail.com>
2020-03-18 00:36:05 +0100

    Add styling to the application.

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <Layout/>
+  <div id="app">
+    <Layout/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,3 +15,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
