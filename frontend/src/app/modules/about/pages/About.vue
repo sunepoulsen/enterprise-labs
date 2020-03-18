9a5e3fa9a94604da5291d308b2c34fa84117124b
sunepoulsen <sunepoulsen@gmail.com>
2020-03-18 01:07:34 +0100

    Small layout changes to module pages.

--- frontend/src/app/modules/about/pages/About.vue
+++ frontend/src/app/modules/about/pages/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <Card>
+      <template slot="title">About Tech Enterprise Labs</template>
+      <template slot="content">
+        Formålet med denne Enterprise applikation er at teste forskellige Enterprise teknologier og mindre features i
+        forskellige forretnings domæner.
+
+        <p>
+          Eksempler på Frontend teknologier:
+        </p>
+        <ul>
+          <li>VUE</li>
+          <li>Typescript</li>
+          <li>PrimeVUE</li>
+          <li>CSS / SCCS</li>
+          <li>...</li>
+        </ul>
+
+        <p>
+          Eksempler på Backend teknologier:
+        </p>
+        <ul>
+          <li>Spring Boot</li>
+          <li>Rest services</li>
+          <li>Docker</li>
+          <li>Kafka</li>
+          <li>Databaser</li>
+          <li>...</li>
+        </ul>
+      </template>
+    </Card>
   </div>
 </template>
