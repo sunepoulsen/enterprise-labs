aa72ddd441d08c03bd34a457e3ffc4ec37ee0a9f
sunepoulsen <sunepoulsen@gmail.com>
2020-06-15 21:29:10 +0200

    Basic structure for component tests

---
+++ backends/labs/timelog-backends/timelog-backend/service/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/timelog/service/ApplicationTest.groovy
@@ -0,0 +1,10 @@
+package dk.sunepoulsen.tech.enterprise.labs.timelog.service
+
+import org.junit.Test
+
+class ApplicationTest {
+    @Test
+    void testGroovyTests() {
+        assert 1 == 1
+    }
+}
